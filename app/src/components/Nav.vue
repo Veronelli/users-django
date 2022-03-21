d626ce497cd821edeb3bf5708a853ad442af7302
vero <vero@debian.fibertel.com.ar>
2022-03-21 15:48:00 -0300

    Add frontend to the project

---
+++ app/src/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="nav justify-content-center|justify-content-end">
+      <a class="nav-link active" href="#">Active link</a>
+      <a class="nav-link" href="#">Link</a>
+      <a class="nav-link disabled" href="#">Disabled link</a>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'HelloWld',
+}
+</script>
